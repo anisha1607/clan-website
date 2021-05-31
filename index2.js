c026689f4880cd4babf89408babeecbbf0fc9650
Anisha Gupta <anishagupta1607@gmail.com>
2021-05-31 16:48:15 +0530

    .

---
+++ index2.js
@@ -0,0 +1,34 @@
+//form validation
+// document.querySelector('.myform').addEventListener('submit',function(event){
+//     alert(1);
+//    //prevents data from being shown on top with file name
+//    event.preventDefault();
+//      //to print input data
+//    const n=event.target.name.value;
+//    const e= event.target.email.value;
+//    const p=event.target.phone.value;
+//    const d=event.target.desc.value;
+   
+ 
+   
+//    if(e.includes('@.')===false){
+//    document.getElementById('xyz').innerHTML='Invalid email id';
+//    }
+//    else{
+//      document.getElementById('xyz').innerHTML='Thank you for submitting the form. We will get back to you soon.';
+     
+   
+//  }
+ 
+ 
+//      //to reset values
+//    event.target.name.value='';
+//    event.target.phone.value='';
+//    event.target.email.value='';
+//    event.target.desc.value='';
+//  })
+
+
+ function abc(){
+    alert('Thank you for submitting the form. We will get back to you soon.');
+ }
